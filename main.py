5ae2dcb1e69ece4010d1b0f7b90091c8ef995e1a
RenanDias12 <52115774+RenanDias12@users.noreply.github.com>
2020-12-05 16:02:58 -0300

    Create project

---
+++ main.py
@@ -0,0 +1,21 @@
+from modules import operations
+
+if __name__ == "__main__":
+    while True:
+        print("\n------------------------------------------------------\n		    MAQUINA VIRTUAL\n------------------------------------------------------\n")
+        comand = ""
+        comands = []
+        while comand != "EXECUTE":
+            comand = input('Insira os comandos: \n')
+            comand = comand.upper()
+            if comand == 'EXIT':
+                break
+            comands.append(comand)
+            
+        if comand != 'EXIT':
+            comands.pop()
+            operation = operations.Operations(comands)
+            if operation.decode() == 0:
+                operation.execute()
+        else:
+            break
---
+++ modules/operations.py
@@ -0,0 +1,192 @@
+
+class Operations:
+    def __init__(self, instructions):
+        
+        self.opcode = []
+        self.instructions = instructions.copy()
+        self.const = 0
+        self.registers = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        print("\n------------------------------------------------------\n		    MAQUINA VIRTUAL\n------------------------------------------------------\n")
+        print("Registradores: ", self.registers)
+
+
+    def decode(self):
+        #Analise de sintaxe e conversão para opcode
+        for inst in self.instructions:
+            if inst == '':
+                print("Comando vazio!")
+                return 1
+
+        for inst in self.instructions:
+            self.code = inst.split()
+
+            if self.code[0] == 'SUMC':
+                self.opcode.append('000')
+            elif self.code[0] == 'MULC':
+                self.opcode.append('001')
+            elif self.code[0] == 'SUMV':
+                self.opcode.append('010')
+            elif self.code[0] == 'MULV':
+                self.opcode.append('011')
+            elif self.code[0] == 'AND':
+                self.opcode.append('100')
+            elif self.code[0] == 'OR':
+                self.opcode.append('101')
+            else:
+                print("Erro de sintaxe!!\n")
+                return 1
+
+            if self.code[1] == 'VAR0':
+                self.opcode.append('0000')
+            elif self.code[1] == 'VAR1':
+                self.opcode.append('0001')
+            elif self.code[1] == 'VAR2':
+                self.opcode.append('0010')
+            elif self.code[1] == 'VAR3':
+                self.opcode.append('0011')
+            elif self.code[1] == 'VAR4':
+                self.opcode.append('0100')
+            elif self.code[1] == 'VAR5':
+                self.opcode.append('0101')
+            elif self.code[1] == 'VAR6':
+                self.opcode.append('0110')
+            elif self.code[1] == 'VAR7':
+                self.opcode.append('0111')
+            elif self.code[1] == 'VAR8':
+                self.opcode.append('1000')
+            else:
+                print("Erro de sintaxe!!\n")
+                return 1
+
+            if self.code[2] == 'VAR0':
+                self.opcode.append('0000')
+            elif self.code[2] == 'VAR1':
+                self.opcode.append('0001')
+            elif self.code[2] == 'VAR2':
+                self.opcode.append('0010')
+            elif self.code[2] == 'VAR3':
+                self.opcode.append('0011')
+            elif self.code[2] == 'VAR4':
+                self.opcode.append('0100')
+            elif self.code[2] == 'VAR5':
+                self.opcode.append('0101')
+            elif self.code[2] == 'VAR6':
+                self.opcode.append('0110')
+            elif self.code[2] == 'VAR7':
+                self.opcode.append('0111')
+            elif self.code[2] == 'VAR8':
+                self.opcode.append('1000')
+            else:
+                print("Erro de sintaxe!!\n")
+                return 1
+
+            if self.code[0] == 'SUMC' or self.code[0] == 'MULC':
+                if int(self.code[3]) > 31:
+                    self.opcode.append(f'{31:05b}')
+                else:
+                    self.opcode.append(f'{int(self.code[3]):05b}')
+            else:
+                if self.code[3] == 'VAR0':
+                    self.opcode.append('00000')
+                elif self.code[3] == 'VAR1':
+                    self.opcode.append('00001')
+                elif self.code[3] == 'VAR2':
+                    self.opcode.append('00010')
+                elif self.code[3] == 'VAR3':
+                    self.opcode.append('00011')
+                elif self.code[3] == 'VAR4':
+                    self.opcode.append('00100')
+                elif self.code[3] == 'VAR5':
+                    self.opcode.append('00101')
+                elif self.code[3] == 'VAR6':
+                    self.opcode.append('00110')
+                elif self.code[3] == 'VAR7':
+                    self.opcode.append('00111')
+                elif self.code[3] == 'VAR8':
+                    self.opcode.append('01000')
+                else:
+                    print("Erro de sintaxe!!\n")
+                    return 1
+        
+        print("Opcodes gerados: ")
+        i=0
+        for PC in range(0, len(self.opcode), 4):
+            print(self.instructions[i], ":", self.opcode[PC],self.opcode[PC+1],self.opcode[PC+2],self.opcode[PC+3], "\n")
+            i += 1
+        return 0
+
+    def execute(self):
+        for PC in range(0, len(self.opcode), 4):
+
+            #Operações
+            if self.opcode[PC] == '000':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                const = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+                self.registers[aux1] = self.registers[aux2] + const
+
+                #Estouro de memória
+                if self.registers[aux1] > 65535:
+                    self.registers[aux1] = 0
+                    print("Memória excedida!")
+                print("Registradores após a operação SUMC: ", self.registers)
+
+            elif self.opcode[PC] == '001':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                const = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+                self.registers[aux1] = self.registers[aux2] * const
+
+                #Estouro de memória
+                if self.registers[aux1] > 65535:
+                    self.registers[aux1] = 0
+                    print("Memória excedida!")
+                print("Registradores após a operação MULC: ", self.registers)
+
+            elif self.opcode[PC] == '010':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                aux3 = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+                self.registers[aux1] = self.registers[aux2] + self.registers[aux3]
+
+                #Estouro de memória
+                if self.registers[aux1] > 65535:
+                    self.registers[aux1] = 0
+                    print("Memória excedida!")
+                print("Registradores após a operação SUMV: ", self.registers)
+                
+            elif self.opcode[PC] == '011':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                aux3 = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+                self.registers[aux1] = self.registers[aux2] * self.registers[aux3]
+
+                #Estouro de memória
+                if self.registers[aux1] > 65535:
+                    self.registers[aux1] = 0
+                    print("Memória excedida!")
+                print("Registradores após a operação MULV: ", self.registers)
+
+            elif self.opcode[PC] == '100':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                aux3 = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+
+                print("Executando operação lógica...")
+                print(f'{self.registers[aux2]:05b}', "AND", f'{self.registers[aux3]:05b}')
+                resp = self.registers[aux2] & self.registers[aux3]
+                self.registers[aux1] = resp
+                print("Registradores após a operação AND: ", self.registers, '\n')
+            
+            elif self.opcode[PC] == '101':
+                aux1 = int(self.opcode[PC+1][3]) + int(self.opcode[PC+1][2])*2 + int(self.opcode[PC+1][1])*4 + int(self.opcode[PC+1][0])*8
+                aux2 = int(self.opcode[PC+2][3]) + int(self.opcode[PC+2][2])*2 + int(self.opcode[PC+2][1])*4 + int(self.opcode[PC+2][0])*8
+                aux3 = int(self.opcode[PC+3][4]) + int(self.opcode[PC+3][3])*2 + int(self.opcode[PC+3][2])*4 + int(self.opcode[PC+3][1])*8 + int(self.opcode[PC+3][0])*16
+
+                print("Executando operação lógica...")
+                print(f'{self.registers[aux2]:05b}', "OR", f'{self.registers[aux3]:05b}')
+                resp = self.registers[aux2] | self.registers[aux3]
+                self.registers[aux1] = resp
+                print("Registradores após a operação OR: ", self.registers, '\n')
+
+        print("Execução finalizada...\n")
